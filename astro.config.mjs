342ff7c63aeef3ca6baa6bdf307d4fcd5876640a
jluo3364 <77076660+jluo3364@users.noreply.github.com>
2024-05-14 21:29:19 -0400

    step 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({site: "https://dark-0-chocolate.netlify.app"});
