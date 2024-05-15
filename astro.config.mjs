77177b5223d56dc9e3a51998d34955fde08464e0
jluo3364 <77076660+jluo3364@users.noreply.github.com>
2024-05-15 12:39:51 -0400

    view transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({site: "https://dark-0-chocolate.netlify.app"});
+export default defineConfig({
+  site: "https://dark-0-chocolate.netlify.app",
+  integrations: [preact()]
+});
